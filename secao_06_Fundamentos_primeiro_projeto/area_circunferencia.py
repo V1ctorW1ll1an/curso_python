ecf5aae38ee56b0ba4580ddf2a3c7749020a10bf
Victor Willian de Sousa <victorwillian1012@gmail.com>
2020-11-08 14:07:47 -0300

    Finalizando projetinho

--- secao_06_Fundamentos_primeiro_projeto/area_circunferencia.py
+++ secao_06_Fundamentos_primeiro_projeto/area_circunferencia.py
@@ -1,6 +1,31 @@
 #!/usr/local/bin/python
+import math
+import sys
+import errno
 
-pi = 3.1415
-raio = 15.3
+ERROR = '\033[91m'
+NORMAL = '\033[0m'
 
-print('area do circulo', pi * raio ** 2)
+
+def help():
+    print("É necessario informar o raio do circulo.")
+    print("Sintaxe: {} <raio>".format(sys.argv[0][2:]))
+
+
+def circulo(raio):
+    return math.pi * float(raio) ** 2
+
+
+if __name__ == '__main__':
+    if len(sys.argv) < 2:
+        help()
+        sys.exit(errno.EPERM)
+
+    if not sys.argv[1].isnumeric():
+        help()
+        print(ERROR, "O raio deve ser um valor numerico", NORMAL)
+        sys.exit(errno.EINVAL)
+
+    raio = sys.argv[1]
+    area = circulo(raio)
+    print('area do circulo: ', area)
